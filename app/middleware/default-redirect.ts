c7bd53bfbac72df0c97fa17897c5006854074a92
Safwan Parker <safwan.parker@ingka.com>
2025-08-02 22:15:22 +0200

    fix: handle default redirection

--- app/middleware/default-redirect.ts
+++ app/middleware/default-redirect.ts
@@ -1,6 +1,13 @@
-export default defineNuxtRouteMiddleware(async (_to, _from) => {
+export default defineNuxtRouteMiddleware(async (to, _from) => {
+  if (to.path === "/login") {
+    return;
+  }
+
   const { data: authCheck } = await useFetch("/api/validate-session");
   if (authCheck.value?.authenticated) {
     return navigateTo("/dashboard/home");
+  }
+  else {
+    return await navigateTo("/login");
   }
 });
