767250ed21c3cd73bf7c6247837260cd3ed15cf3
Safwan Parker <safwan.parker@ingka.com>
2025-08-07 19:09:39 +0200

    chore: show admin secret after creating consumer

--- app/components/dashboard/DashboardAddConsumerDrawer.vue
+++ app/components/dashboard/DashboardAddConsumerDrawer.vue
@@ -25,14 +25,6 @@ const state = reactive<DashboardAddConsumerFormSchema>({
   rateLimit: 0
 });
 
-const resetState = () => {
-  state.name = "";
-  state.description = "";
-  state.allowedHosts = [];
-  state.isActive = true;
-  state.rateLimit = 0;
-};
-
 const updateHost = (index: number, value: string) => {
   if (state.allowedHosts) {
     state.allowedHosts[index] = value;
@@ -56,6 +48,19 @@ function onError(event: FormErrorEvent) {
   });
 }
 
+const apiSecretState = reactive({
+  value: "Create Consumer to see the API Secret"
+});
+
+const resetState = () => {
+  state.name = "";
+  state.description = "";
+  state.allowedHosts = [];
+  state.isActive = true;
+  state.rateLimit = 0;
+  apiSecretState.value = "Create Consumer to see the API Secret";
+};
+
 async function onSubmit(_event: FormSubmitEvent<DashboardAddConsumerFormSchema>) {
   const consumer = state as Consumer;
   await consumerStore.createNew({ data: consumer });
@@ -67,13 +72,19 @@ async function onSubmit(_event: FormSubmitEvent<DashboardAddConsumerFormSchema>)
     color: "success",
     duration: 10000
   });
+
+  apiSecretState.value = consumerStore.selected.data.apiSecret;
 }
+
+const { copy: copyApiSecret, copied: copyApiSecretCopied } = useClipboard();
 </script>
 
 <template>
   <UDrawer
     v-model:open="drawerOpen"
     direction="right"
+    :handle-only="true"
+    @close="resetState"
   >
     <UButton
       label="Create Consumer"
@@ -83,111 +94,142 @@ async function onSubmit(_event: FormSubmitEvent<DashboardAddConsumerFormSchema>)
     />
 
     <template #content>
-      <UForm
-        :schema="schema"
-        :state="state"
-        class="w-full flex flex-col gap-3 p-5"
-        @submit="onSubmit"
-        @error="onError"
-      >
-        <h2 class="text-2xl font-bold">
-          Create Consumer
-        </h2>
-        <div class="flex flex-row gap-5 justify-evenly">
-          <UFormField
-            label="Name"
-            name="name"
-            class="w-full"
-            required
-          >
-            <UInput
-              v-model="state.name"
-              class="w-full"
-            />
-          </UFormField>
-
-          <UFormField
-            label="Description"
-            name="description"
-            class="w-full"
-          >
-            <UInput
-              v-model="state.description"
-              class="w-full"
-            />
-          </UFormField>
-        </div>
-
-        <div class="flex flex-row justify-evenly w-full">
-          <UFormField
-            label="Allowed Hosts (e.g. www.example.com, example.com, etc.)"
-            name="allowedHosts"
-            class="w-full"
-          >
-            <div class="space-y-2">
-              <div
-                v-for="(host, index) in state.allowedHosts"
-                :key="index"
-                class="flex items-center gap-2"
-              >
-                <UInput
-                  :model-value="host"
-                  placeholder="https://example.com"
-                  class="flex-1"
-                  @update:model-value="updateHost(index, $event)"
-                />
-                <UButton
-                  icon="i-heroicons-trash"
-                  color="error"
-                  variant="ghost"
-                  size="sm"
-                  @click="removeHost(index)"
-                />
-              </div>
-              <UButton
-                icon="i-heroicons-plus"
-                variant="outline"
-                size="sm"
-                @click="addHost"
-              >
-                Add Host
-              </UButton>
-            </div>
-          </UFormField>
-        </div>
-
-        <div class="flex flex-row justify-evenly gap-5 w-full">
-          <UFormField
-            label="Rate Limit"
-            name="rateLimit"
-          >
-            <UInput
-              v-model="state.rateLimit"
-              class="w-full"
-              type="number"
-              min="0"
-            />
-          </UFormField>
-
-          <UFormField
-            label="Consumer Status"
-            name="isActive"
-            class="w-full"
-          >
-            <USwitch
-              v-model="state.isActive"
-              class=""
-            />
-          </UFormField>
-        </div>
-
-        <UButton
-          type="submit"
-          class="cursor-pointer"
+      <div class="w-full h-full flex flex-col gap-5 p-5">
+        <UForm
+          :schema="schema"
+          :state="state"
+          class="w-full flex flex-col gap-3"
+          @submit="onSubmit"
+          @error="onError"
         >
-          Submit
-        </UButton>
-      </UForm>
+          <h2 class="text-2xl font-bold">
+            Create Consumer
+          </h2>
+          <div class="flex flex-row gap-5 justify-evenly">
+            <UFormField
+              label="Name"
+              name="name"
+              class="w-full"
+              required
+            >
+              <UInput
+                v-model="state.name"
+                class="w-full"
+              />
+            </UFormField>
+
+            <UFormField
+              label="Description"
+              name="description"
+              class="w-full"
+            >
+              <UInput
+                v-model="state.description"
+                class="w-full"
+              />
+            </UFormField>
+          </div>
+
+          <div class="flex flex-row justify-evenly w-full">
+            <UFormField
+              label="Allowed Hosts (e.g. www.example.com, example.com, etc.)"
+              name="allowedHosts"
+              class="w-full"
+            >
+              <div class="space-y-2">
+                <div
+                  v-for="(host, index) in state.allowedHosts"
+                  :key="index"
+                  class="flex items-center gap-2"
+                >
+                  <UInput
+                    :model-value="host"
+                    placeholder="https://example.com"
+                    class="flex-1"
+                    @update:model-value="updateHost(index, $event)"
+                  />
+                  <UButton
+                    icon="i-heroicons-trash"
+                    color="error"
+                    variant="ghost"
+                    size="sm"
+                    @click="removeHost(index)"
+                  />
+                </div>
+                <UButton
+                  icon="i-heroicons-plus"
+                  variant="outline"
+                  size="sm"
+                  @click="addHost"
+                >
+                  Add Host
+                </UButton>
+              </div>
+            </UFormField>
+          </div>
+
+          <div class="flex flex-row justify-evenly gap-5 w-full">
+            <UFormField
+              label="Rate Limit"
+              name="rateLimit"
+            >
+              <UInput
+                v-model="state.rateLimit"
+                class="w-full"
+                type="number"
+                min="0"
+              />
+            </UFormField>
+
+            <UFormField
+              label="Consumer Status"
+              name="isActive"
+              class="w-full"
+            >
+              <USwitch
+                v-model="state.isActive"
+                class=""
+              />
+            </UFormField>
+          </div>
+
+          <UButton
+            type="submit"
+            class="cursor-pointer"
+          >
+            Submit
+          </UButton>
+        </UForm>
+        <span>
+          Make sure to copy the Admin Secret below, as it will not be shown again.
+        </span>
+        <UInput
+          v-model="apiSecretState.value"
+          disabled
+          class="w-full"
+          :ui="{ trailing: 'pr-0.5' }"
+        >
+          <template
+            v-if="apiSecretState.value.length"
+            #trailing
+          >
+            <UTooltip
+              text="Copy to clipboard"
+              :content="{ side: 'right' }"
+            >
+              <UButton
+                :color="copyApiSecretCopied ? 'success' : 'neutral'"
+                variant="link"
+                size="sm"
+                :icon="copyApiSecretCopied ? 'i-lucide-copy-check' : 'i-lucide-copy'"
+                class="bg-muted"
+                aria-label="Copy to clipboard"
+                @click="copyApiSecret(apiSecretState.value)"
+              />
+            </UTooltip>
+          </template>
+        </UInput>
+      </div>
     </template>
   </UDrawer>
 </template>
